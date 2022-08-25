3e9799901104bceb7329128b1636867773d1748d
Andrei O <andrei@flashsoft.eu>
2022-08-26 01:39:02 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Clear Wallet',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
